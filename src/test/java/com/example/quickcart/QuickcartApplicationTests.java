115e4d2374d57354674d20864778765215efb050
saabith <e21342@eng.pdn.ac.lk>
2026-04-20 05:49:39 +0530

    Initial Commit

---
+++ src/test/java/com/example/quickcart/QuickcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quickcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickcartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
