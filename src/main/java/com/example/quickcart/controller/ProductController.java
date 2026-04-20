0c714ff4abd507c05bf801833fb225e27ef2be36
saabith <e21342@eng.pdn.ac.lk>
2026-04-20 18:56:49 +0530

    create the product controller and service

---
+++ src/main/java/com/example/quickcart/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.example.quickcart.controller;
+
+import com.example.quickcart.entity.Product;
+import com.example.quickcart.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+}
